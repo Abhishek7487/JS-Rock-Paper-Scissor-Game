26314ffb3fd27f6ca5a1d39779f9a0796449a7e1
Abhishek7487 <dellabhishek7487@gmail.com>
2023-02-11 09:04:45 +0530

    Updated code files to make game work properly for each condition

--- js/main.js
+++ js/main.js
@@ -1,36 +1,82 @@
 // Elements
-const userScoreEl = document.querySelector(".userscore");
-const compScoreEl = document.querySelector(".compscore");
+const userScoreValEl = document.querySelector("#userScoreVal");
+const compScoreValEl = document.querySelector("#compScoreVal");
 const userStatEl = document.querySelector("#result-user-stat");
 const compStatEl = document.querySelector("#result-comp-stat");
 const finalStatEL = document.querySelector("#result-final-stat");
+// Setting Initial Scores to 0
+userScoreValEl.textContent = 0;
+compScoreValEl.textContent = 0;
 
-// Role Images
-const rockImg = document.querySelector("[alt='rock']");
-const paperImg = document.querySelector("[alt='paper']");
-const scissorImg = document.querySelector("[alt='scissors']");
+// Role Elements
+const rockRoleEl = document.querySelector("#r");
+const paperRoleEl = document.querySelector("#p");
+const scissorRoleEl = document.querySelector("#s");
 
-const rolesArr = [rockImg, paperImg, scissorImg];
+// Creating spam tag for each Result and appending it.
+const spanUserRole = document.createElement("span");
+spanUserRole.textContent = "";
+userStatEl.appendChild(spanUserRole);
+
+const spanCompRole = document.createElement("span");
+spanCompRole.textContent = "";
+compStatEl.appendChild(spanCompRole);
+
+const spanWinner = document.createElement("span");
+spanWinner.textContent = "";
+finalStatEL.appendChild(spanWinner);
+
+// USER selects his role and the random role for COMP is generated
+const rolesArr = [rockRoleEl, paperRoleEl, scissorRoleEl];
 rolesArr.forEach((role) => {
   role.addEventListener("click", () => {
-    let userRole = role;
-    let compRole = getRandomCompRole();
+    const userRole = role;
+    // Displaying srlected role infront of USER stat
+    if (userRole === rockRoleEl) {
+      spanUserRole.textContent = "Rock";
+    } else if (userRole == paperRoleEl) {
+      spanUserRole.textContent = "Paper";
+    } else if (userRole == scissorRoleEl) {
+      spanUserRole.textContent = "Scissor";
+    }
 
-    let userRoleClone = userRole.cloneNode("true");
-    let compRoleClone = compRole.cloneNode("true");
-    userRoleClone.classList.add("resultImg");
-    compRoleClone.classList.add("resultImg");
+    // Displaying srlected role infront of COMP stat
+    const compRole = getRandomCompRole();
+    if (compRole === rockRoleEl) {
+      spanCompRole.textContent = "Rock";
+    } else if (compRole == paperRoleEl) {
+      spanCompRole.textContent = "Paper";
+    } else if (compRole == scissorRoleEl) {
+      spanCompRole.textContent = "Scissor";
+    }
 
-    let spanUserRole = document.createElement("span");
-    spanUserRole.appendChild(userRoleClone);
-    userStatEl.appendChild(spanUserRole);
-    let spanCompRole = document.createElement("span");
-    spanCompRole.appendChild(compRoleClone);
-    compStatEl.appendChild(spanCompRole);
+    // Comparing Roles for finding WINNER or DRAW
+    if (
+      (userRole === rockRoleEl && compRole === scissorRoleEl) ||
+      (userRole === paperRoleEl && compRole === rockRoleEl) ||
+      (userRole === scissorRoleEl && compRole === paperRoleEl)
+    ) {
+      spanWinner.textContent = " User 👤";
+      userScoreValEl.textContent++;
+    } else if (
+      (userRole === paperRoleEl && compRole === scissorRoleEl) ||
+      (userRole === scissorRoleEl && compRole === rockRoleEl) ||
+      (userRole === rockRoleEl && compRole === paperRoleEl)
+    ) {
+      spanWinner.textContent = " Computer 🖥️";
+      compScoreValEl.textContent++;
+    } else if (
+      (userRole === rockRoleEl && compRole === rockRoleEl) ||
+      (userRole === paperRoleEl && compRole === paperRoleEl) ||
+      (userRole === scissorRoleEl && compRole === scissorRoleEl)
+    ) {
+      spanWinner.textContent = " DRAW 📍";
+    }
   });
 });
 
+// Function for Generating random COMP role
 function getRandomCompRole() {
-  const roleImgsArr = [rockImg, paperImg, scissorImg];
+  const roleImgsArr = [rockRoleEl, paperRoleEl, scissorRoleEl];
   return roleImgsArr[Math.floor(Math.random() * roleImgsArr.length)];
 }
